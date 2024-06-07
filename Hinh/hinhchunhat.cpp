b903b0ddb90aab14530771023ae8553406a1dfec
baizmeoq <baizmeoq@gmail.com>
2024-06-07 02:20:35 -0700

    add

---
+++ Hinh/hinhchunhat.cpp
@@ -0,0 +1,30 @@
+//Ve hinh chu nhat rong
+#include <iostream>
+using namespace std;
+
+void drawHollowSquare(int size){
+	for(int i=0; i<size; i++){
+    	for(int j=0; j<size; j++){
+        	if(i==0 || i==size-1)
+            	cout<<"* ";
+        	else if(j==0 || j==size-1)
+            	cout<<"* ";
+        	else
+            	cout<<"  ";
+    	}
+    	cout<<endl;
+	}
+}
+
+int main()
+{
+	int size;
+	cout <<"Nhập kích thước hình vuông: "; cin>>size;
+    
+	if(size>1){
+    	drawHollowSquare(size);
+	}else{
+    	cout<<"Kích thước phải lớn hơn 1 để tạo hình vuông rỗng"<<endl;
+	}
+	return 0;
+}
